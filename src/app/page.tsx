9f96bfded97e58af21addb8e34bc37a21e77aa7f
Derrick <pshubomi@gmail.com>
2025-01-28 05:06:55 +0100

    working on the header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,55 @@
 import Image from "next/image";
+import { LuPhone } from "react-icons/lu";
+import { TbClockHour8 } from "react-icons/tb";
+import { CgArrowLongLeftL } from "react-icons/cg";
+import Link from "next/link";
+import { IoIosSquare } from "react-icons/io";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    <main>
+      <header className="flex h-20">
+        <h2 className="font-mono w-[100px] flex justify-center items-center text-2xl font-semibold">
+          LOGO
+        </h2>
+        <nav className="w-full">
+          <div className=" flex gap-3 border-b-2 p-1 pl-5">
+            <div className="flex gap-2 items-center">
+              <LuPhone /> +234 916 1965 510
+            </div>
+            <div className="flex gap-2 items-center">
+              <TbClockHour8 /> Mon - Fri 7:30am - 6:30pm
+            </div>
+          </div>
+          <div className="p-1 pl-5 flex items-center justify-between">
+            <ul className="flex gap-4 items-center">
+              <li>
+                <Link href="#" className="flex items-center gap-2">
+                  <CgArrowLongLeftL /> Home
+                </Link>
+              </li>
+              <li>
+                <Link href="#" className="flex items-center gap-2">
+                  <IoIosSquare /> About
+                </Link>
+              </li>
+              <li>
+                <Link href="#" className="flex items-center gap-2">
+                  <IoIosSquare /> Career
+                </Link>
+              </li>
+              <li>
+                <Link href="#" className="flex items-center gap-2">
+                  <IoIosSquare /> Contact
+                </Link>
+              </li>
+            </ul>
+            <button className="py-2 px-5 border border-amber-700 text-amber-700 font-semibold">
+              Free consulation
+            </button>
+          </div>
+        </nav>
+      </header>
+    </main>
   );
 }
